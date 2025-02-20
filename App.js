cff689a5bc733c44d47589b224c9894e990e1e98
김동규 <dgorzldd@gmail.com>
2025-02-21 00:17:33 +0900

    Animation 1 - React State에 두지 않기 / Animated.value를 직접 수정 못한다 / component를 AnimatedComponent로 바꿔야 한다

--- App.js
+++ App.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Animated } from "react-native";
 import styled from "styled-components/native";
 
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
 export default function App() {
-  return null;
+  const Y = new Animated.Value(0);
+  const moveUp = () => {};
+  return (
+    <Container>
+      <AnimatedBox
+        onPress={moveUp}
+        style={{ transform: [{ translateY: Y }] }}
+      />
+    </Container>
+  );
 }
